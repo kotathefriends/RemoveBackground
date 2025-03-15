b931e3ba0f2c8158c3803ddf9b2de26854dcf244
kotathefriends <5070kouta@gmail.com>
2025-03-15 14:36:59 +0900

    Initial Commit

---
+++ RemoveBackground/RemoveBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoveBackgroundApp.swift
+//  RemoveBackground
+//
+//  Created by KOTA TAKAHASHI on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct RemoveBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
